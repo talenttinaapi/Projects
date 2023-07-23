0c963713d92de9bf72c06fed5dfadf75bc24a782
talenttinaapi <ttinaapi88@gmail.com>
2023-07-23 20:33:24 +0200

    weatherapi

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+from pprint import pprint
+
+API_key = '3b5661e75aad06cc37f3303b11629429'
+
+city = input("Enter a city: ")
+
+base_url = "https://api.openweathermap.org/data/2.5/weather?appid=" +API_key+ "&q="+city
+
+weather_data = requests.get(base_url).json()
+
+pprint(weather_data)
