f040e541fdedc7e56a8cfa3a550aca82cb9652ec
talenttinaapi <ttinaapi88@gmail.com>
2023-07-23 16:37:50 +0200

    code

--- conditional_code.py
+++ conditional_code.py
@@ -1,6 +1,9 @@
 balance = 5000
-money = 150
+money = 15000
 if balance > money:
     balance -= money
     print(f"Current balance is {balance}")
     print("Transaction complete, Dont forget your card")
+else:
+    print("Insufficient funds")
+    print(f"Current balance is {balance}")
