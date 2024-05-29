01230bd6c455008320995cd4b5cb71258e647b75
talent tinaapi <ttinaapi88@gmail.com>
2024-05-29 12:36:27 +0200

    python math

--- function.py
+++ function.py
@@ -2,6 +2,6 @@ def calculate_square_area(side: int):
     return side * side
 
 area = calculate_square_area(5)
-print(f"The area of the sqaure is {area}")
+print(f"The area of the square is {area}, and the value of pi is {math.pi}")
 
 //Math.PI//
