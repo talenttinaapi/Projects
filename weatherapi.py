b0218aa4d9eebb8b7b698d480af4e8330d9b8e7a
Talent Tinaapi <talenttinaapi@Talents-MacBook-Pro.local>
2024-09-30 21:33:34 +0200

    py

--- weatherapi.py
+++ weatherapi.py
@@ -10,3 +10,12 @@ base_url = "https://api.openweathermap.org/data/2.5/weather?appid=" +API_key+ "&
 weather_data = requests.get(base_url).json()
 
 pprint(weather_data)
+
+import pandas as pd
+
+# Read data from a CSV file
+data = pd.read_csv('data.csv')
+
+# Perform basic analysis
+mean = data['column_name'].mean()
+print(f"Mean: {mean}")
